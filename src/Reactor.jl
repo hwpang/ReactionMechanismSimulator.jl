5dbbd2cbc8f95cd780e7cf86348136377380489f
Hao-Wei Pang <hwpang@mit.edu>
2021-12-20 21:18:28 -0500

    Add VaporLiquidMassTransferInternalInterface

--- src/Reactor.jl
+++ src/Reactor.jl
@@ -487,6 +487,8 @@ end
             evaluate(inter,dydt,domains,vT[inter.domaininds[1]],vT[inter.domaininds[2]],vphi[inter.domaininds[1]],vphi[inter.domaininds[2]],vGs[inter.domaininds[1]],vGs[inter.domaininds[2]],cstot,p)
         elseif isa(inter,DiffusiveInternalInterface)
             evaluate(inter,dydt,vV[inter.domaininds[1]],vV[inter.domaininds[2]],vT[inter.domaininds[1]],vT[inter.domaininds[2]],cstot,p)
+        elseif isa(inter,VaporLiquidMassTransferInternalInterface)
+            evaluate(inter,dydt,vV[inter.domaininds[1]],vV[inter.domaininds[2]],vT[inter.domaininds[1]],vT[inter.domaininds[2]],cstot,p)
         end
     end
     for (i,domain) in enumerate(domains)
