b0e67fe6061bad7f87b49a8282fbef9f74332c97
Hao-Wei Pang <hwpang@mit.edu>
2021-12-22 17:24:43 -0500

    debug VLMT

--- src/Reactor.jl
+++ src/Reactor.jl
@@ -140,6 +140,15 @@ function Reactor(domains::T,y0s::W,tspan::W2,interfaces::Z=Tuple(),ps::X=DiffEqB
             inter.parameterindexes[2] = length(p)+length(ps[i+length(domains)])
             inter.rxnarray .= getinterfacereactioninds(inter.domain1,inter.domain2,inter.reactions)
             p = vcat(p,ps[i+length(domains)])
+        elseif isa(inter, VaporLiquidMassTransferInternalInterface)
+            ind1 = findfirst(isequal(inter.domain1),domains)
+            ind2 = findfirst(isequal(inter.domain2),domains)
+            inter.domaininds[1] = ind1
+            inter.domaininds[2] = ind2
+            inter.parameterindexes[1] = length(p)+1
+            inter.parameterindexes[2] = length(p)+length(ps[i+length(domains)])
+            inter.rxnarray .= getinterfacemasstransferinds(inter.domain1,inter.domain2,inter.masstransferspcnames)
+            p = vcat(p,ps[i+length(domains)])
         end
     end
     
