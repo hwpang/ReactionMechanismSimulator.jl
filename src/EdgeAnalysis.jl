5da30b8db6a62db6af6eb3df5df89bc389dd62bd
Hao-Wei Pang <hwpang@mit.edu>
2021-12-30 13:04:29 -0500

    fix

--- src/EdgeAnalysis.jl
+++ src/EdgeAnalysis.jl
@@ -671,7 +671,7 @@ function identifyobjects!(sim,corespcsinds,corerxninds,edgespcsinds,
                 if !(obj in newobjects || obj in invalidobjects)
                     push!(tempnewobjects,obj)
                     push!(tempnewobjectinds,ind)
-                    push!(tempnewobjectvals,bnum)
+                    push!(tempnewobjectvals,lr)
                     push!(tempnewobjecttype,"connecting")
                 end
             end
