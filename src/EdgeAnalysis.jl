b1468afac17ca38d7730335778a57e19b3c65ae8
Hao-Wei Pang <hwpang@mit.edu>
2022-01-14 23:41:25 -0500

    fix

--- src/EdgeAnalysis.jl
+++ src/EdgeAnalysis.jl
@@ -666,6 +666,8 @@ function identifyobjects!(sim,corespcsinds,corerxninds,edgespcsinds,
             if lr > lossratiotolerance
                 obj = sim.reactions[ind]
                 if !(obj in newobjects || obj in invalidobjects)
+                    @info "edgerxninds for connecting"
+                    @info ind
                     push!(tempnewobjects,obj)
                     push!(tempnewobjectinds,ind)
                     push!(tempnewobjectvals,lr)
@@ -790,9 +792,6 @@ function identifyobjects!(sim,corespcsinds,corerxninds,edgespcsinds,
             end
         end
     end
-
-    @info "identifyobjects! output"
-    @info (terminated,interrupt,conversion)
     
     return (terminated,interrupt,conversion) 
 end
