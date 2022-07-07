4a2cb0ed96b6f3c0ae58a6f93deb7aaf22de4f53
Hao-Wei Pang <hwpang@mit.edu>
2022-07-07 14:16:50 -0400

    fix

--- src/fluxdiagrams.jl
+++ src/fluxdiagrams.jl
@@ -53,7 +53,7 @@ function drawspc(spc::Species,path::String=".")
     else
         figure(figsize=(1,1))
         plot()
-        annotate(name,xy=(0.5, 0.5), ha="center", size=16)
+        annotate("nc7h16",xy=(0.5, 0.5), ha="center", xycoords="axes fraction", size=16)
         axis("off")
         savefig(joinpath(path,fname))
     end
